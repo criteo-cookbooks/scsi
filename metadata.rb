8f63d8f7f8d9f87a1007bdba370518515903eb6a
Jeremy MAURO <j.mauro@criteo.com>
2018-01-30 16:25:50 +0100

    Add travis-ci

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'scsi'
+maintainer       'SRE Core Infra team'
+maintainer_email 'sre-core-infra@criteo.com'
+license          'All rights reserved'
+description      'Pseudo-ohai for scsi'
+long_description 'Pseudo-ohai for scsi'
+issues_url       'https://github.com/criteo-cookbooks/scsi/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/criteo-cookbooks/scsi.git' if respond_to?(:source_url)
+version          '0.0.0'
+supports         'centos'
+supports         'windows'
